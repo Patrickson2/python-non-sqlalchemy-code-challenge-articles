413218893770da4961346d7122a44443751e3535
Patrickson2 <thairupatrickson@gmail.com>
2025-11-19 16:01:52 +0300

    genarated article to pass tests

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,47 @@
 class Article:
+    # i want to include all article instances by creating an empty list first
+    all = []
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        # here i want to validate the author first 
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            self.author = None
+
+    # validating instances in the magazine
+        if isinstance(magazine, Magazine):
+            self.magazine = magazine
+        else:
+            self.magazine = None
+
+    # validating instances in the title
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            title = "Untitled"
+        self._title = title
+
+    # Wanting to add an article to the list --implement the .append formart.
+        Article.all.append(self)
+        
+    # adding of the property 
+    @property
+    def title(self):
+        # Title is immutable
+        return self._title
+
+    @title.setter
+    def title(self, value):
+
+        if isinstance(value, str) and 5 <= len(value) <= 50:
+            self._title = value    
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        # # Name must be a string with a length of not long than 0
+        # if isinstance(name, str) and len(name) > 0:
+        #     self._name = name
+        # else:
+        #     self._name = "Anonymous"
+        # self.name = name
 
     def articles(self):
         pass

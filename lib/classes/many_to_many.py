4177afe207482fb2e3992ea3343e2513e02cec95
Patrickson2 <thairupatrickson@gmail.com>
2025-11-19 18:02:00 +0300

    initiated prop method to return category magazine

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -125,7 +125,16 @@ class Magazine:
     def name(self, value):
         if isinstance(value, str) and 2 <= len(value) <= 16:
             self._name = value
-                
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+                        
     def articles(self):
         pass
 
